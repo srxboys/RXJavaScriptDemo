349e97b9b4f24f9df791cbf47501864ecc414bae
srxboys <srxboys@126.com>
2017-05-25 12:22:45 +0800

    webView 我的项目 就是这么写的

--- RXJavaScriptDemo/RXJSToHtmlBlockViewController.m
+++ RXJavaScriptDemo/RXJSToHtmlBlockViewController.m
@@ -12,9 +12,12 @@
 #import "RXJSToHtmlBlockViewController.h"
 
 #import <JavaScriptCore/JavaScriptCore.h>
+#import "UIWebView+TS_JavaScriptContext.h"
 
 @interface RXJSToHtmlBlockViewController ()
-
+{
+    JSContext * _jsContext;
+}
 @end
 
 @implementation RXJSToHtmlBlockViewController
@@ -24,9 +27,62 @@
     [self settingHtmlLocalCodeWithType:HTMLTypeForth];
 }
 
+- (void)webView:(UIWebView *)webView didCreateJavaScriptContext:(JSContext *)ctx {
+    dispatch_time_t delayTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.2/*延迟执行时间*/ * NSEC_PER_SEC));
+    dispatch_after(delayTime, dispatch_get_main_queue(), ^{
+        [self addJavaScriptName:@"londingJS"];
+        _jsContext = [self.webView valueForKeyPath:@"documentView.webView.mainFrame.javaScriptContext"];
+        JSValue * app = [_jsContext objectForKeyedSubscript:@"app"];
+        app[@"londingJS"] = ^(id obj) {
+            NSLog(@"londingJS=%@", obj);
+        };
+        
+        NSLog(@"--register-->%zd",[[NSDate date] timeIntervalSince1970]);
+        [self addJSAlterPrint];
+    });
+}
+
+- (void)addJavaScriptName:(NSString *)functionName {
+    
+    NSString * jsString = [NSString stringWithFormat:@"var script = document.createElement('script');"
+                           "script.type = 'text/javascript';"
+                           "script.text = \"var app = {}; app.%@ = function() {};\";"
+                           "document.getElementsByTagName('head')[0].appendChild(script);", functionName];
+    
+    [self.webView stringByEvaluatingJavaScriptFromString:jsString];
+    
+}
+
+
 - (void)webViewDidFinishLoad:(UIWebView *)webView {
     [super webViewDidFinishLoad:webView];
-//    [self addJSAlterPrint];
+    NSLog(@"--webViewDidFinishLoad-->%zd",[[NSDate date] timeIntervalSince1970]);
+    
+    //注入js 方法
+    [self addJavaScriptName:@"share"];
+    
+    _jsContext = [self.webView valueForKeyPath:@"documentView.webView.mainFrame.javaScriptContext"];
+    
+    //获取 app变量的share方法
+    __weak typeof(self)weakSelf = self;
+    JSValue * app = [_jsContext objectForKeyedSubscript:@"app"];
+    
+    app[@"share"] = ^(id obj){
+        [weakSelf webShare:obj];
+    };
+    
+    [self addJSAlterPrint];
+}
+
+- (void)webShare:(id)obj {
+//    NSData *data = [(NSString *)obj dataUsingEncoding:NSUTF8StringEncoding ];
+//    NSDictionary *dict = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
+    //上面是对json的处理
+    
+    //对象、变量的 处理
+    NSLog(@"obj=%@", obj);
+    
+    //看html 给你什么类型了
 }
 
 - (void)didReceiveMemoryWarning {
